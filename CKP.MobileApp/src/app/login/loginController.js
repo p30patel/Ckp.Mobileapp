5627dd31db337f222945130f7ed6b7d68d70f7ae
p30patel <patelp@USNBPATELP7.checkpt.com>
2015-08-27 12:47:57 -0400

    login

--- CKP.MobileApp/src/app/login/loginController.js
+++ CKP.MobileApp/src/app/login/loginController.js
@@ -69,6 +69,7 @@ app.controller('loginController', [
                        $scope.loginData = {
                            userName: "",
                            password: "",
+                           useRefreshTokens : true
                        };
                        $scope.translations = {};
                        $scope.message = "";
