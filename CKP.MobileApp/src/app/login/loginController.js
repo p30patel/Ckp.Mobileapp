4833c2293eff20ae9f01e2da82586adbbc6e1a0a
p30patel <patelp@USNBPATELP7.checkpt.com>
2015-09-09 14:02:50 -0400

    push

--- CKP.MobileApp/src/app/login/loginController.js
+++ CKP.MobileApp/src/app/login/loginController.js
@@ -262,7 +262,8 @@ app.controller('loginController', [
                                   function (data) {
                                       alert('Register success');
                                       var currentDevice = el.push.currentDevice();
-                                      alert(currentDevice);
+                                      var d = JSON.stringify(currentDevice);
+                                      alert('Device: ' + d + '\nId' + d.id);
                                       el.push.getRegistration(onDeviceSuccess, onDeviceError);
                                       var onDeviceSuccess = function (r) {
                                           alert('s' + JSON.stringify(r));
