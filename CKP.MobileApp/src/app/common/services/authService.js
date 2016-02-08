043113c65c08422f7686e5b90c76c44460483697
p30patel <patelp@USNBPATELP7.checkpt.com>
2016-02-08 17:12:00 -0500

    update

--- CKP.MobileApp/src/app/common/services/authService.js
+++ CKP.MobileApp/src/app/common/services/authService.js
@@ -117,7 +117,8 @@ app.factory('authService', [
                             alert(loginData.userName.toLocaleLowerCase() +' == ' + organizationDetail.UserName.toLocaleLowerCase());
                             var hasSameUUID = angular.equals(uuId, organizationDetail.DeviceId);
                             alert(uuId + ' = ' + organizationDetail.DeviceId);
-                            alert(new Date().getDate() + ' == ' + refreshData.date);
+                            var dt = new Date();
+                            alert(dt .getDate()+ ' == ' + refreshData.date);
                             var hasSameDate = false;
                             //var hasSameDate = angular.equals(new Date().toLocaleDateString(), refreshData.date);
                             
