b74ebb93d7db4d99905fc945d2582a3e7539ad20
p30patel <p30patel@yahoo.com>
2016-03-30 15:33:20 -0400

    Implemented truning on notification by default.

--- CKP.MobileApp/src/app/common/services/homeDataService.js
+++ CKP.MobileApp/src/app/common/services/homeDataService.js
@@ -5,171 +5,163 @@ app.factory("homeDataService", [
                 "$http", "$q", "localStorageService", "ngAuthSettings", "authService", "timeStampService", "$rootScope",
 function ($http, $q, localStorageService, ngAuthSettings, authService, timeStampService, $rootScope) {
 
-                    var authServiceBase = ngAuthSettings.authServiceBaseUri;
-                   
-                    var homeDataServiceFactory = {};
-                    var date = kendo.toString(new Date(), "yyyy-MM-dd HH");
-                   
-                    var forceGetOrderCounts = function (data) {
-                        var deferred = $q.defer();
+    var authServiceBase = ngAuthSettings.authServiceBaseUri;
 
-                        var organizationDetail = localStorageService.get('organizationDetail');
-                       
-                        var jsonIn = {
-                            OrderNumber: data.OrderNumber,
-                            ShoppingCartId: data.ShoppingCartId,
-                            SalesOrderNumber: data.SalesOrderNumber,
-                            VendorRef: data.VendorRef,
-                            OrganizationDetail: organizationDetail,
-                            SearchType: data.SearchType
-                        }
-                     
-                     
-                        var url = authServiceBase + "webapi/api/core/MobileApp/GetOrderCounts";
-                        $http.post(url, jsonIn).success(function (result) {
-                            
-                            $rootScope.hasPreviousSearch = $rootScope.hasSearchOrApporval;
-                            $rootScope.hasSearchOrApporval = false;
-                            $rootScope.timeStampOrderCount = new Date().getTime();
-                            console.log('order count loaded from server');
-                            localStorageService.set('orderCounts', result);
+    var homeDataServiceFactory = {
+    };
+    var date = kendo.toString(new Date(), "yyyy-MM-dd HH");
 
-                            deferred.resolve(result);
-                          
-                        }).error(function (err, status) {
-                            deferred.reject(err);
-                        
-                        });
-                        
+    var forceGetOrderCounts = function (data) {
+        var deferred = $q.defer();
 
-                        return deferred.promise;
-                    };
-                    
-                    var getOrderCounts = function (data) {
-                        var deferred = $q.defer();
-                        var organizationDetail = localStorageService.get('organizationDetail');
-                        var orderCounts = localStorageService.get('orderCounts');
-                        var hasForceRefresh = true;
-                        var timeNow = new Date().getTime();
-                        var persistTime = 1000 * 60 * 2;
-                        
-                        if (orderCounts !== null && !$rootScope.hasSearchOrApporval && !$rootScope.hasPreviousSearch)
-                        {
-                            if (persistTime && timeNow > Number($rootScope.timeStampOrderCount) + persistTime) {
-                                hasForceRefresh = true;
-                            }
-                        else{
-                            hasForceRefresh = false;
-                        }
-                        }
-                        
-                        if (!hasForceRefresh) {
-                            console.log('order count loaded from local' + orderCounts);
-                            deferred.resolve(orderCounts);
-                        }
-                        else {
-                            forceGetOrderCounts(data).then(function (result) {
-                                deferred.resolve(result);
-                            });
-                        }
-                       
+        var organizationDetail = localStorageService.get('organizationDetail');
 
-                        if (typeof (window.navigator.simulator) === 'undefined') {
-                            window.plugins.EqatecAnalytics.Monitor.TrackFeature("search.SearchType." + data.SearchType);
-                        }
-                        return deferred.promise;
-                    };
+        var jsonIn = {
+            OrderNumber: data.OrderNumber,
+            ShoppingCartId: data.ShoppingCartId,
+            SalesOrderNumber: data.SalesOrderNumber,
+            VendorRef: data.VendorRef,
+            OrganizationDetail: organizationDetail,
+            SearchType: data.SearchType
+        }
 
-                    var forceGetOrderSummary = function (jsonIn) {
-                        var deferred = $q.defer();
-                     
-                        var url = authServiceBase + "webapi/api/core/MobileApp/GetOrderSummary";
 
-                        var organization = localStorageService.get('organizationDetail');
-                        if (!organization) {
-                        }
+        var url = authServiceBase + "webapi/api/core/MobileApp/GetOrderCounts";
+        $http.post(url, jsonIn).success(function (result) {
 
-                        jsonIn.UserId = organization.UserId;
-                     
-                        $http.post(url, jsonIn).success(function (result) {
-                         
-                            deferred.resolve(result);                           
-                        }).error(function (xhr, status, error) {   
-                           
-                            deferred.reject(error);
-                      
-                        });
+            $rootScope.hasPreviousSearch = $rootScope.hasSearchOrApporval;
+            $rootScope.hasSearchOrApporval = false;
+            $rootScope.timeStampOrderCount = new Date().getTime();
+            console.log('order count loaded from server');
+            localStorageService.set('orderCounts', result);
 
-                        return deferred.promise;
-                    };
+            deferred.resolve(result);
 
-                    var getOrderSummary = function (jsonIn) {
-                        var deferred = $q.defer();
-                       
-                        forceGetOrderSummary(jsonIn).then(function (result) {
-                                deferred.resolve(result);
-                            });
-                        return deferred.promise;
-                    };
+        }).error(function (err, status) {
+            deferred.reject(err);
 
-                    var setDefaultNotifcations = function () {
-                        var deferred = $q.defer();
-                        var userProfileData = localStorageService.get('user-profile');
-                      
-                        if (typeof userProfileData.HasTrunOnNotifcation !== 'undefined' || !userProfileData.HasTrunOnNotifcation) {
+        });
 
-                            var organizationDetail = localStorageService.get('organizationDetail');
-                            var userId = organizationDetail.UserId;
-                            var url = authServiceBase + "webapi/api/core/MobileApp/SetDefaultNotification?userId=" + userId;
-                            $http.post(url).success(function (result) {
-                              
-                                if (typeof userProfileData === 'undefined')
-                                {
-                                    var userProfile = {
-                                        SearchType: 'OrderNumber',
-                                        SelectedPara: '1',
-                                        HasReadNote: true,
-                                        IsFirtTime: true,
-                                        HasTrunOnNotifcation: true,
-                                        HasForceClearAll: false
-                                    };
-                                 
-                                    localStorageService.set('user-profile', userProfile);
-                                }
-                                else {
-                                    userProfileData.HasTrunOnNotifcation = true;
-                                    localStorageService.set('user-profile', userProfileData);
-                                }
-                               
-                                var userProfileData = localStorageService.get('user-profile');
-                                console.log(userProfileData);
-                            
-                                deferred.resolve(result);
 
-                            }).error(function (err, status) {
-                                deferred.reject(err);
+        return deferred.promise;
+    };
 
-                            });
-                          
-                        }
-                        else {
-                         
-                            deferred.resolve('success');
-                           
-                        }
+    var getOrderCounts = function (data) {
+        var deferred = $q.defer();
+        var organizationDetail = localStorageService.get('organizationDetail');
+        var orderCounts = localStorageService.get('orderCounts');
+        var hasForceRefresh = true;
+        var timeNow = new Date().getTime();
+        var persistTime = 1000 * 60 * 2;
 
-                        return deferred.promise;
-                       
+        if (orderCounts !== null && !$rootScope.hasSearchOrApporval && !$rootScope.hasPreviousSearch) {
+            if (persistTime && timeNow > Number($rootScope.timeStampOrderCount) + persistTime) {
+                hasForceRefresh = true;
+            }
+            else {
+                hasForceRefresh = false;
+            }
+        }
+
+        if (!hasForceRefresh) {
+            console.log('order count loaded from local' + orderCounts);
+            deferred.resolve(orderCounts);
+        }
+        else {
+            forceGetOrderCounts(data).then(function (result) {
+                deferred.resolve(result);
+            });
+        }
+
+
+        if (typeof (window.navigator.simulator) === 'undefined') {
+            window.plugins.EqatecAnalytics.Monitor.TrackFeature("search.SearchType." + data.SearchType);
+        }
+        return deferred.promise;
+    };
+
+    var forceGetOrderSummary = function (jsonIn) {
+        var deferred = $q.defer();
+
+        var url = authServiceBase + "webapi/api/core/MobileApp/GetOrderSummary";
+
+        var organization = localStorageService.get('organizationDetail');
+        if (!organization) {
+        }
+
+        jsonIn.UserId = organization.UserId;
+
+        $http.post(url, jsonIn).success(function (result) {
+
+            deferred.resolve(result);
+        }).error(function (xhr, status, error) {
+
+            deferred.reject(error);
+
+        });
+
+        return deferred.promise;
+    };
+
+    var getOrderSummary = function (jsonIn) {
+        var deferred = $q.defer();
+
+        forceGetOrderSummary(jsonIn).then(function (result) {
+            deferred.resolve(result);
+        });
+        return deferred.promise;
+    };
+
+    var setDefaultNotifcations = function () {
+        var deferred = $q.defer();
+        var userProfileData = localStorageService.get('user-profile');
+
+        var organizationDetail = localStorageService.get('organizationDetail');
+
+        if ((typeof userProfileData.HasTrunOnNotifcation !== 'undefined' || !userProfileData.HasTrunOnNotifcation) && organizationDetail) {
+
+            var url = authServiceBase + "webapi/api/core/MobileApp/SetDefaultNotification";
+            $http.post(url, organizationDetail).success(function (result) {
+                if (result === 'success') {
+                    if (typeof userProfileData === 'undefined') {
+                        var userProfile = {
+                            SearchType: 'OrderNumber',
+                            SelectedPara: '1',
+                            HasReadNote: true,
+                            IsFirtTime: true,
+                            HasTrunOnNotifcation: true,
+                            HasForceClearAll: false
+                        };
+                        localStorageService.set('user-profile', userProfile);
                     }
-
-                    homeDataServiceFactory.getOrderCounts = getOrderCounts;
-                    homeDataServiceFactory.forceGetOrderCounts = forceGetOrderCounts;
-
-                    homeDataServiceFactory.getOrderSummary = getOrderSummary;
-                    homeDataServiceFactory.forceGetOrderSummary = forceGetOrderSummary;
-
-                    homeDataServiceFactory.setDefaultNotifcations = setDefaultNotifcations;
-
-                    return homeDataServiceFactory;
+                    else {
+                        userProfileData.HasTrunOnNotifcation = true;
+                        localStorageService.set('user-profile', userProfileData);
+                    }
                 }
-            ]);
+                deferred.resolve(result);
+
+            }).error(function (err, status) {
+                deferred.reject(err);
+            });
+        }
+        else {
+            deferred.resolve('success');
+        }
+
+        return deferred.promise;
+
+    };
+
+
+    homeDataServiceFactory.getOrderCounts = getOrderCounts;
+    homeDataServiceFactory.forceGetOrderCounts = forceGetOrderCounts;
+
+    homeDataServiceFactory.getOrderSummary = getOrderSummary;
+    homeDataServiceFactory.forceGetOrderSummary = forceGetOrderSummary;
+
+    homeDataServiceFactory.setDefaultNotifcations = setDefaultNotifcations;
+
+    return homeDataServiceFactory;
+}
+]);
--- CKP.MobileApp/src/app/common/services/notificationDataService.js
+++ CKP.MobileApp/src/app/common/services/notificationDataService.js
@@ -12,10 +12,14 @@ app.factory("notificationDataService", [
                         var authentication = authService.authentication;
                        
                         var organizationDetail = localStorageService.get('organizationDetail');
-                        var userId = organizationDetail.UserId;
-                        $http.get(authServiceBase + 'webapi/api/core/MobileApp/GetUserNotifications?userId=' + userId).success(function (result) {
-
-                            result = checkNotifcationByPermisson(result);
+                     
+                        var orgContext = '';
+                        if (!organizationDetail)
+                        {
+                                deferred.reject('Error while getting data');
+                        }
+                        $http.post(authServiceBase + 'webapi/api/core/MobileApp/GetUserNotificationSettings', organizationDetail).success(function (result) {
+                           
                             deferred.resolve(result);
                         })
                             .error(function (err, status) {
@@ -24,78 +28,7 @@ app.factory("notificationDataService", [
                         return deferred.promise;
                     };
 
-                    var checkNotifcationByPermisson = function(result){
-
-                        var organizationDetail = localStorageService.get('organizationDetail');
-                        var orderCounts = localStorageService.get('orderCounts');
-                        var newResults = result;
-                        if (organizationDetail)
-                        {
-                            newResults = [];
-                            angular.forEach(result, function (value, key) {
-                              
-                                switch (value.SubscriptionType)
-                                {
-                                    case 101:
-                                        
-                                        if (orderCounts.MobileOrderCountList[0].HasStagedOrder)
-                                        {
-                                         
-                                            newResults.push(value);                                          
-                                        }
-                                        break;
-
-                                 
-                                    case 102: 
-                                    case 103:                                        
-                                        if (orderCounts.MobileOrderCountList[0].HasApproval)
-                                        {
-                                            
-                                            newResults.push(value);
-                                            
-                                        }
-                                        break;
-                                    case 104:
-                                        {
-                                            newResults.push(value);
-
-                                        }
-                                        break;
-
-                                    case 105:
-                                        if (organizationDetail.OrgContext.EnableShipmentDeliveryInfo)
-                                        {
-
-                                            newResults.push(value);
-
-                                        }
-                                        break;
-                                        
-                                    case 106:
-                                        {
-                                            newResults.push(value);
-
-                                        }
-                                        break;
-
-                                    case 107:
-                                        {
-                                            newResults.push(value);
-
-                                        }
-                                        break;
-
-                                    default:
-                                        break;
-                                }
-                              
-                                
-                            });
-                        }
-                       
-                        return newResults;
-                    }
-
+                 
                     var updateNotification = function (notifcationData) {
                         var deferred = $q.defer();
                         var authServiceBase = ngAuthSettings.authServiceBaseUri;
