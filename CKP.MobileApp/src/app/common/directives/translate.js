cf97c8dade68b699ca8cb4a96462f8346714d82b
p30patel <p30patel@yahoo.com>
2016-04-12 13:59:52 -0400

    Inbox delete

--- CKP.MobileApp/src/app/common/directives/translate.js
+++ CKP.MobileApp/src/app/common/directives/translate.js
@@ -3,15 +3,35 @@ app.directive('translateElement', function (translateService) {
         link: function(scope, element, attrs) {
             attrs.$observe('translateElement', function (resourceName) {
 
-                var additionaTranslate = element.attr('data-translateText');               
-                if (resourceName) {
-                    if (typeof additionaTranslate !== 'undefined') {
-                        resourceName = resourceName + additionaTranslate;
-                    }
+                var additionalTranslate = element.attr('data-translateText');
 
-             
+                if (additionalTranslate == 'SubscriptionType')
+                {
+                    var currentSubscriptionType = element.attr('data-SubscriptionType');
+                    //check resoruce name is exisited -- then set the text
+                    var subscriptionType = [
+                        { id: 101, description: "NewOrderReceived" },
+                        { id: 102, description: "OrderApproval" },
+                        { id: 103, description: "ApprovedOrDeclinedOrder" },
+                        { id: 104, description: "ShipmentConfirmation" },
+                        { id: 105, description: "DeliveryConfirmation" },
+                        { id: 106, description: "PrintshopHoliday" },
+                        { id: 107, description: "Maintenance" },
+                        { id: 108, description: "OrderStatusChanged" }
+                    ];
+                   
+                    angular.forEach(subscriptionType, function (value, key) {
+                        if (value.id == resourceName) {
+                            resourceName = value.description;
+                            additionalTranslate = '';
+                        }
+                    });
+                }
+                if (resourceName) {
+                    if (typeof additionalTranslate !== 'undefined') {
+                        resourceName = resourceName + additionalTranslate;
+                    }
                     resourceName = translateService.getResourceValue(resourceName);
-                    
                 }
               
                 element.text(resourceName == undefined ? '' : resourceName);               
--- CKP.MobileApp/src/app/notification/inboxController.js
+++ CKP.MobileApp/src/app/notification/inboxController.js
@@ -8,6 +8,10 @@ function ($scope, $http, $sce, translateService, authService, notificationDataSe
     $scope.form.title.resoruceName = "Inbox";
     $scope.form.title.resoruceValue = translateService.getResourceValue($scope.form.title.resoruceName);
 
+    $scope.form.viewAll = {};
+    $scope.form.viewAll.resoruceName = "View Next";
+    $scope.form.viewAll.resoruceValue = translateService.getResourceValue($scope.form.viewAll.resoruceName);
+
     $scope.beforeShow = function () {
         kendo.mobile.application.showLoading();
         if (!authService.authentication.isAuth) {
@@ -19,9 +23,9 @@ function ($scope, $http, $sce, translateService, authService, notificationDataSe
     };
     $scope.message = "";
     $scope.notifications = [];
-    $scope.PageSize = 30;
+    $scope.PageSize = 1;
     $scope.CurrentPage = 1;
-
+    $scope.hasNext = false;
     $scope.afterShow = function (e) {
 
         var view = kendo.mobile.application.view();
@@ -48,20 +52,39 @@ function ($scope, $http, $sce, translateService, authService, notificationDataSe
 
     setResources();
    
-    var getInboxMessages = function () {
+    var getInboxMessages = function (hasNext) {
         kendo.mobile.application.showLoading();
 
         if (typeof (window.navigator.simulator) === 'undefined') {
             window.plugins.EqatecAnalytics.Monitor.TrackFeature("method.getInbox");
+        }
+        if (hasNext)
+        {
+            $scope.CurrentPage++;
         }
         var jsonIn = {
             PageSize: $scope.PageSize,
             PageNumber: $scope.CurrentPage,
             UserId: 0,
         };
-        notificationDataService.getInboxMessages(jsonIn).then(function (result) {
-            kendo.mobile.application.hideLoading();
-            $scope.notifications = result;
+        notificationDataService.getInboxMessages(jsonIn).then(function (result) {           
+            kendo.mobile.application.hideLoading();           
+            if (hasNext)
+            {
+                var currentNotifications = $scope.notifications;
+              
+                angular.forEach(result, function (value, key) {
+                    if (key <= result.length) {
+                        currentNotifications.push(value);
+                    }
+                });
+
+                $scope.notifications = currentNotifications;
+            }
+            else {
+                $scope.notifications = result;
+            }
+            $scope.hasNext = result.length >= $scope.PageSize;
         }).catch(function (error) {
             kendo.mobile.application.hideLoading();
             $scope.message = "Faild to get notifcation.";
@@ -71,19 +94,55 @@ function ($scope, $http, $sce, translateService, authService, notificationDataSe
 
         });
     }
-    getInboxMessages();
+    getInboxMessages(false);
+
+    $scope.delete = function (e, id) {
+        var jsonIn = {
+            Id: id,
+            Status: 105,
+            HasDelete: true
+        };
+        updateInbox(jsonIn);
+    }
 
 
+    var markAsRead = function (e) {
+        alert('in');
+        //var jsonIn = {
+        //    Id: id,
+        //    Status: 103,
+        //    HasDelete: false
+        //};
+
+        //updateInbox(jsonIn);
+        // $('#pushTitleId-' + id).removeClass('km-bold-font');
+
+    }
     $scope.myTouch = {
         filter: ">li",
         enableSwipe: true,
-       
+        touchstart: function (e) {
+            console.log(e.originalEvent.pageX)
+            $('.swipe-delete li > a.open').css('left', '0px').removeClass('open') // close em all
+            $(e.currentTarget).addClass('open')
+            x = e.originalEvent.targetTouches[0].pageX // anchor point
+        },
+        touchend: function (e) {
+            e.preventDefault()
+            $(this).parents('li').slideUp('fast', function () {
+                $(this).remove()
+            })
+        },
         swipe: function (e) {
+         
             var button = kendo.fx($(e.touch.currentTarget).find("[data-role=button]"));
            
             
             if (e.direction === 'left') {
-                button.expand().duration(60).play();
+                if (e.direction === "left") {
+                    var del = e.sender.element;
+                    kendo.fx(del).slideIn("right").duration(500).reverse();
+                }
             }
             else {
                 $(e.touch.currentTarget).find("[data-role=button]:visible").hide();
@@ -104,10 +163,9 @@ function ($scope, $http, $sce, translateService, authService, notificationDataSe
                 if (jsonIn.HasDelete)
                 {
                     //mark as delete
+                    $('#pushId-' + jsonIn.id).addClass('ck-hide');
                 }
-                else {
-                    // mark as read
-                }
+                
             }
         }).catch(function (error) {
 
@@ -118,26 +176,10 @@ function ($scope, $http, $sce, translateService, authService, notificationDataSe
 
         });
     }
-    $scope.delete = function(e, id)
-    {
-        var jsonIn = {
-            Id: id,
-            Status: 105,
-            HasDelete : true
-        };
-        updateInbox(jsonIn);
+   
+    $scope.ViewMore = function () {
+        getInboxMessages(true);
     }
-
-    $scope.read = function(e, id)
-    {
-        var jsonIn = {
-            Id: id,
-            Status: 103,
-            HasDelete: false
-        };
-        updateInbox(jsonIn);
-    }
-
     $scope.renderHtml = function (message) {
 
         return $sce.trustAsHtml(message);
